c58b480845f6daae32b9290ff93098569e0902bf
MonicaOduor <Monicaoduor2@gmail.com>
2022-02-16 08:37:01 +0300

    Updates

---
+++ precache-manifest.88759fc279aad8d8edb24df77377d7fb.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6945fb83dc7476317ed62d06675ed32",
+    "url": "/grocery/index.html"
+  },
+  {
+    "revision": "2219ae9f54069e1bc1f0",
+    "url": "/grocery/static/css/main.dfb7f8dd.chunk.css"
+  },
+  {
+    "revision": "206df2cef3d073d0a019",
+    "url": "/grocery/static/js/2.622c7b28.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/grocery/static/js/2.622c7b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2219ae9f54069e1bc1f0",
+    "url": "/grocery/static/js/main.539ba4b6.chunk.js"
+  },
+  {
+    "revision": "c6675946b4c339e6af34",
+    "url": "/grocery/static/js/runtime-main.2d7d60e1.js"
+  },
+  {
+    "revision": "cdf7e9ad7b964886aceeacd4c8c9310d",
+    "url": "/grocery/static/media/aboutUsImg.cdf7e9ad.png"
+  },
+  {
+    "revision": "536f768848bfcc8baef4155eab9311a9",
+    "url": "/grocery/static/media/arrowroot.536f7688.png"
+  },
+  {
+    "revision": "e8761b1a810a280867ecb28e75aacf02",
+    "url": "/grocery/static/media/avocado.e8761b1a.png"
+  },
+  {
+    "revision": "0800ffd56815e9e7fc86d341e3721980",
+    "url": "/grocery/static/media/babyspinach.0800ffd5.png"
+  },
+  {
+    "revision": "a5e7558999ef19d233e2fc680b5319df",
+    "url": "/grocery/static/media/banana.a5e75589.png"
+  },
+  {
+    "revision": "6dda1096cd9eb2cfc41b26ee61b2877b",
+    "url": "/grocery/static/media/beetroot.6dda1096.png"
+  },
+  {
+    "revision": "2c58079b7a3b0ba29c91d2d1b017e321",
+    "url": "/grocery/static/media/bgimage.2c58079b.jpg"
+  },
+  {
+    "revision": "66d805d72e93db1735316940abe0533a",
+    "url": "/grocery/static/media/cabbage.66d805d7.png"
+  },
+  {
+    "revision": "e565a6398edd40a263ad74459dd44160",
+    "url": "/grocery/static/media/carrot.e565a639.png"
+  },
+  {
+    "revision": "c311e622380d06594947e76fca811fcb",
+    "url": "/grocery/static/media/cassava.c311e622.png"
+  },
+  {
+    "revision": "2023e16c8adb53a3ee8cc2809093952d",
+    "url": "/grocery/static/media/celery.2023e16c.png"
+  },
+  {
+    "revision": "0290cc573835f6d10918db639badd393",
+    "url": "/grocery/static/media/coconut.0290cc57.png"
+  },
+  {
+    "revision": "20e9d0d94dfcd47bb41e54409d77e4a9",
+    "url": "/grocery/static/media/cucumber.20e9d0d9.png"
+  },
+  {
+    "revision": "1b54dcc3276ee25c44f86c277f50944c",
+    "url": "/grocery/static/media/garlic.1b54dcc3.png"
+  },
+  {
+    "revision": "8a7c213c73b39a35cc19e2d5330eaff7",
+    "url": "/grocery/static/media/ginger.8a7c213c.png"
+  },
+  {
+    "revision": "470708ee2ee88bbce51298a720c58567",
+    "url": "/grocery/static/media/grapes.470708ee.png"
+  },
+  {
+    "revision": "2e769b6388955903f06fc8e132aacf90",
+    "url": "/grocery/static/media/greenbanana.2e769b63.png"
+  },
+  {
+    "revision": "56f72b17d1385206047dfa9ca82b4248",
+    "url": "/grocery/static/media/greenpepper.56f72b17.png"
+  },
+  {
+    "revision": "7c6316cdc58ada2fb083e0499282eb08",
+    "url": "/grocery/static/media/home-card-img-2.7c6316cd.jpg"
+  },
+  {
+    "revision": "c87d3c54b45879524ff1d17d0828c406",
+    "url": "/grocery/static/media/kale.c87d3c54.png"
+  },
+  {
+    "revision": "3890ac1f2f15d5276f243a4cc1386581",
+    "url": "/grocery/static/media/kiwi.3890ac1f.png"
+  },
+  {
+    "revision": "1c77391d16c6a11af7db08394b7f4279",
+    "url": "/grocery/static/media/logo.1c77391d.png"
+  },
+  {
+    "revision": "da974d67e4a91678bad1462a0d3aa730",
+    "url": "/grocery/static/media/mango.da974d67.png"
+  },
+  {
+    "revision": "af1867fcf82ded9a14b4a4e6b1e88c55",
+    "url": "/grocery/static/media/mint.af1867fc.png"
+  },
+  {
+    "revision": "014b24c63281bc3a7b43958f25663d4b",
+    "url": "/grocery/static/media/onions.014b24c6.png"
+  },
+  {
+    "revision": "a648476099d3142c2955180971ac2992",
+    "url": "/grocery/static/media/pepper.a6484760.png"
+  },
+  {
+    "revision": "a684128d2967311be86fe7a64df376b7",
+    "url": "/grocery/static/media/pineapple.a684128d.png"
+  },
+  {
+    "revision": "338af228a7233131c3f1695f3f16bee9",
+    "url": "/grocery/static/media/potato.338af228.png"
+  },
+  {
+    "revision": "5c5069e51a1ff37fb34d3ab9b36fb4cd",
+    "url": "/grocery/static/media/redbellpepper.5c5069e5.png"
+  },
+  {
+    "revision": "e060fa350dbefe82448fe20f5282e964",
+    "url": "/grocery/static/media/redchilli.e060fa35.png"
+  },
+  {
+    "revision": "7196187e95cfa8ba33dca67f0dbc5899",
+    "url": "/grocery/static/media/spinach.7196187e.png"
+  },
+  {
+    "revision": "e6e3b071679a95638de83e427bff4a55",
+    "url": "/grocery/static/media/strawberry.e6e3b071.png"
+  },
+  {
+    "revision": "451c4ae384e75ef9104e33ba71e7ed29",
+    "url": "/grocery/static/media/sweetpotato.451c4ae3.png"
+  },
+  {
+    "revision": "80e917556a00d7efecbf5a1bc913675b",
+    "url": "/grocery/static/media/terere.80e91755.png"
+  },
+  {
+    "revision": "942370b7b3ceeadb773431ddfe777f1c",
+    "url": "/grocery/static/media/tomato.942370b7.png"
+  },
+  {
+    "revision": "3c79e141d420bf9a695921b01bad2b09",
+    "url": "/grocery/static/media/watermelon.3c79e141.png"
+  },
+  {
+    "revision": "3c49e5bdc7fba9beecd48b6a2aa044e6",
+    "url": "/grocery/static/media/yellowbellpepper.3c49e5bd.png"
+  }
+]);
